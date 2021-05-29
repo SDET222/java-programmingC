f2ce7b50200a1b827e24d099bd2f554d827622e3
Orkhan <jkrogers222@gmail.com>
2021-05-29 15:26:01 -0400

    list in class

--- src/day48_constructor_static/CybertekGroups.java
+++ src/day48_constructor_static/CybertekGroups.java
@@ -1,6 +1,45 @@
 package day48_constructor_static;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CybertekGroups {
 
+    public static void main(String[] args) {
+
+
+        Group group1 = new Group("Cyberbugs");
+
+
+        System.out.println(group1.getMembers().size());
+
+        group1.addMember("Trump");
+        group1.addMember("Clinton");
+        group1.addMember("Obama");
+
+        System.out.println(group1.getMembers());
+
+        System.out.println(group1.getMembers().size());
+        System.out.println("group1 = " + group1);
+
+        Group group2 = new Group("CyberCats");
+
+        group2.setMembers(new ArrayList<>(Arrays.asList("Ron","John","Don","Andrea")));
+
+        group2.removeMember("Don");
+
+        System.out.println(group2.getMembers());
+
+        if (group2.getMembers().contains("Ron")) {
+            System.out.println("Ron is a member of group2");
+        } else {
+            System.out.println("Ron is not a member of group2");
+
+        }
+
+
+
+
+    }
 
 }
