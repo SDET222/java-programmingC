75f73f2e5da1a292d4bddb39e842e66a1f21e835
Orkhan <jkrogers222@gmail.com>
2021-05-29 11:34:33 -0400

    object created

--- src/day48_constructor_static/CustomerObjects.java
+++ src/day48_constructor_static/CustomerObjects.java
@@ -4,9 +4,22 @@ public class CustomerObjects {
     public static void main(String[] args) {
 
 
-        Customer john = new Customer();
+        Customer cs1 = new Customer();
 
+        System.out.println(cs1.toString());
 
+        cs1.setId("A22");
+        cs1.setName("John");
+        System.out.println("============================");
+        System.out.println(cs1);
+
+        Customer cs2 = new Customer("Mike", "A33"); // create object and assign values in same statement
+
+        System.out.println(cs2);
+
+        Customer cs3 = new Customer("Murodil", "A789");
+
+        System.out.println(cs3);
 
 
 
