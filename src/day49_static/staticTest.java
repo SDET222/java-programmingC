bfea3ed197985a4d96eab970f2e11af6452ff486
Orkhan <jkrogers222@gmail.com>
2021-06-01 21:56:31 -0400

    static

---
+++ src/day49_static/staticTest.java
@@ -0,0 +1,28 @@
+package day49_static;
+
+public class staticTest {
+
+    public static void main(String[] args) {
+
+
+
+     StaticMethods.displayMessage("Ferrari");
+
+     StaticMethods stm = new StaticMethods();
+
+     stm.instanceMethod();
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
