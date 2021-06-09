b28ea814724df4b8fe0f2328e1eea438e648af3b
Orkhan <jkrogers222@gmail.com>
2021-06-08 22:07:46 -0400

    end of today's class

---
+++ src/day51_inheritance/super_keyword/LyftXl.java
@@ -0,0 +1,14 @@
+package day51_inheritance.super_keyword;
+
+public class LyftXl extends Lyft {
+
+    @Override
+    public double calculateRate(double miles) {
+
+
+        return super.calculateRate(miles) * 1.1;
+
+    }
+
+
+}
