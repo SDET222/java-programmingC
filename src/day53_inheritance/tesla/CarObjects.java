eaffbbb138acd280548ffe05937591eb79ab632f
Orkhan <jkrogers222@gmail.com>
2021-06-13 11:44:19 -0400

    car class and object created

---
+++ src/day53_inheritance/tesla/CarObjects.java
@@ -0,0 +1,28 @@
+package day53_inheritance.tesla;
+
+public class CarObjects {
+    public static void main(String[] args) {
+
+        ElectricCar ec1 = new ElectricCar("Tesla","Model Y",48190.0,2021,326);
+
+        ec1.drive(50);
+
+        System.out.println(ec1);
+       // System.out.println(ec1.make); Error: make is private
+        System.out.println("ec1 make =" + ec1.getMake());
+        System.out.println("ec1 model =" + ec1.getModel());
+        System.out.println("ec1 range = "+ ec1.getRange());
+        System.out.println("ec1 year = "+ ec1.getYear());
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
