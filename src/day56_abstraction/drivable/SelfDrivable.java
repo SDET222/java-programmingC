0bc3f612940993b9a7b79cf3f8a5f512a7572935
Orkhan <jkrogers222@gmail.com>
2021-06-22 22:00:51 -0400

    interface done

--- src/day56_abstraction/drivable/SelfDrivable.java
+++ src/day56_abstraction/drivable/SelfDrivable.java
@@ -3,7 +3,9 @@ package day56_abstraction.drivable;
 public interface SelfDrivable {
 
  void autoPiloting();
-
+public default void selfPark() { // only for interface!
+ System.out.println("Self parking in progress...");
+}
 
 
 }
