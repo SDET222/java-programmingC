aeca974ea8cffafc9c3424345c6f004035e7817a
Orkhan <jkrogers222@gmail.com>
2021-06-27 14:45:07 -0400

    more practice

--- src/day57_abstraction_polymorphism/abstract_clas_vs_interface/AbstractA.java
+++ src/day57_abstraction_polymorphism/abstract_clas_vs_interface/AbstractA.java
@@ -2,13 +2,17 @@ package day57_abstraction_polymorphism.abstract_clas_vs_interface;
 
 public abstract class AbstractA {
 
+    public AbstractA(){
+        System.out.println("Constructor from AbstractA...");
+    }
+
     int num1;
     private double price;
     public static int count;
     public final String TYPE = "abstract type";
     public static final String LANGUAGE = "java";
 
-    public abstract void abstrMethodA();
+    public abstract void abstrMethodA(); // can not be private or final or static !
     public void normalMethodB(){
         System.out.println("Method B is non-abstract...called now");
     }
--- src/day57_abstraction_polymorphism/abstract_clas_vs_interface/InterfaceA.java
+++ src/day57_abstraction_polymorphism/abstract_clas_vs_interface/InterfaceA.java
@@ -1,6 +1,10 @@
 package day57_abstraction_polymorphism.abstract_clas_vs_interface;
 
-public interface InterfaceA {
+public interface InterfaceA { // interface can extend only Interface
+
+//    public InterfaceA() {      interface can not have constructor
+//
+//    }
 
     public static final String TYPE = "interface type";
     double MAX_COUNT  = 222.2; //automatically --> public static final
