79ac808b692b8d33113da05ee93e050e038dd7b6
Orkhan <jkrogers222@gmail.com>
2021-06-29 21:48:09 -0400

    jhjg

--- src/day58_polymorphism/SuperManTest.java
+++ src/day58_polymorphism/SuperManTest.java
@@ -12,6 +12,15 @@ public class SuperManTest {
         father.playWithKid();
         father.riseKid();
 
+        SuperMan spMan3 = new SuperMan();
+        spMan3.getPaid();
+        spMan3.work("Scrum master");
+        spMan3.feedKid();
+        spMan3.playWithKid();
+        spMan3.riseKid();
+
+
+
 
 
 
