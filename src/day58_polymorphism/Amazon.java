d2d45fab8d6404fc1507163cdc322816dce54cdc
Orkhan <jkrogers222@gmail.com>
2021-06-29 21:56:59 -0400

    online shop

---
+++ src/day58_polymorphism/Amazon.java
@@ -0,0 +1,24 @@
+package day58_polymorphism;
+
+public class Amazon extends OnlineShop implements Prime{
+
+    @Override
+    public void buy() {
+
+    }
+
+    @Override
+    public void sell() {
+
+    }
+
+    @Override
+    public void ship() {
+
+    }
+
+    @Override
+    public void primeShipping() {
+
+    }
+}
---
+++ src/day58_polymorphism/OnlineShop.java
@@ -0,0 +1,13 @@
+package day58_polymorphism;
+
+public abstract class OnlineShop {
+
+    public abstract void buy();
+    public abstract void sell();
+    public abstract void ship();
+
+
+
+
+
+}
