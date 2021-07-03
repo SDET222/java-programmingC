348043300ea94741a4bab1f2a11daba8909cbd10
Orkhan <jkrogers222@gmail.com>
2021-07-03 11:56:46 -0400

    WebElement interface and classes created

---
+++ src/day59_polymorphism_exeptions/InputField.java
@@ -0,0 +1,23 @@
+package day59_polymorphism_exeptions;
+
+public class InputField implements WebElement {
+
+    public void getValue() {
+
+    }
+
+    @Override
+    public void sendKeys(String txt) {
+
+    }
+
+    @Override
+    public void click() {
+
+    }
+
+    @Override
+    public void getText() {
+
+    }
+}
---
+++ src/day59_polymorphism_exeptions/Link.java
@@ -0,0 +1,23 @@
+package day59_polymorphism_exeptions;
+
+public class Link implements WebElement{
+
+    public void getLinkHref() {
+
+    }
+
+    @Override
+    public void sendKeys(String txt) {
+
+    }
+
+    @Override
+    public void click() {
+
+    }
+
+    @Override
+    public void getText() {
+
+    }
+}
