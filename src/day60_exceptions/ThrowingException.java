d4f88ad150a073e0072d6423acf71573ec9dc312
Orkhan <jkrogers222@gmail.com>
2021-07-06 22:06:34 -0400

    expt

--- src/day60_exceptions/ThrowingException.java
+++ src/day60_exceptions/ThrowingException.java
@@ -1,4 +1,18 @@
 package day60_exceptions;
 
 public class ThrowingException {
+    public static void main(String[] args) {
+        System.out.println("Throwing exceptions is not hard, lets create exceptions");
+        RuntimeException e = new RuntimeException();
+       // throw e;
+        String userName="";
+        if(userName.isEmpty()) {
+
+            throw new RuntimeException("UserName can not be empty><");
+        }
+
+
+
+
+    }
 }
