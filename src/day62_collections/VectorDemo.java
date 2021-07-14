c1110bc2260f81c0fcce99124cb15795470856cf
Orkhan <jkrogers222@gmail.com>
2021-07-13 22:38:46 -0400

    vector

---
+++ src/day62_collections/VectorDemo.java
@@ -0,0 +1,29 @@
+package day62_collections;
+
+import java.util.List;
+import java.util.*;
+
+public class VectorDemo {
+    public static void main(String[] args) {
+
+        Vector<Integer> nums = new Vector<>();
+        List<String> states = new Vector<>();
+
+        states.add("NJ");
+        states.add("NY");
+        states.add("MA");
+        states.add("TX");
+        states.add("FL");
+        states.add("FL");
+        System.out.println(states);
+
+
+
+
+
+
+
+
+
+    }
+}
