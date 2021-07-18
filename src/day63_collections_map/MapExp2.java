5aaf4c60f4df034673ef2424ab4d6d655df92fc9
Orkhan <jkrogers222@gmail.com>
2021-07-18 14:19:11 -0400

    hashMap

---
+++ src/day63_collections_map/MapExp2.java
@@ -0,0 +1,14 @@
+package day63_collections_map;
+
+import java.util.HashMap;
+import java.util.*;
+public class MapExp2 {
+    public static void main(String[] args) {
+
+        // id, Person
+
+        Map<Integer,Person> man = new HashMap<>();
+
+
+    }
+}
